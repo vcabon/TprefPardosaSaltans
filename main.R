1f75a9474531307e49c96cceb4f195e90c2ba4fa
Valentin Cabon <valcabon@univ-rennes1.fr>
2023-04-12 16:46:54 +0200

    minor updates

--- main.R
+++ main.R
@@ -2,10 +2,29 @@
 source("utils/libraries.R")
 
 # load data sets
-test1=read.table("data/DATA_PARDOSA_2.txt", h=T, na.string="NA", stringsAsFactors = T, dec=".") #Tpref data
-d_indiv=read.table("data/DONNEES_indiv.txt", h=T, na.string="NA", stringsAsFactors = T, dec=",") #individuals information
-length_cephalothorax=read.table("data/length_cephalothorax.txt", h=T, na.string="NA", stringsAsFactors = T, dec=".") #morphological measurements (body size)
-length_legs=read.table("data/length_legs.txt", h=T, na.string="NA", stringsAsFactors = T, dec=".") #morphological measurements (legs length)
+test1 = read.table("data/DATA_PARDOSA_2.txt", 
+                 h = T, 
+                 na.string = "NA", 
+                 stringsAsFactors = T, 
+                 dec = ".") #Tpref data
+
+d_indiv = read.table("data/DONNEES_indiv.txt", 
+                   h = T, 
+                   na.string = "NA", 
+                   stringsAsFactors = T, 
+                   dec = ",") #individuals information
+
+length_cephalothorax = read.table("data/length_cephalothorax.txt", 
+                                  h = T, 
+                                  na.string = "NA", 
+                                  stringsAsFactors = T, 
+                                  dec = ".") #morphological measurements (body size)
+
+length_legs = read.table("data/length_legs.txt", 
+                         h=T, 
+                         na.string="NA", 
+                         stringsAsFactors = T, 
+                         dec=".") #morphological measurements (legs length)
 
 # Tpref dynamic over the 90min experiment
 source("scripts/test_dynamic_tpref_90min_with557ind.R") # formatting data and run models
--- scripts/figures_factors_Tpref.R
+++ scripts/figures_factors_Tpref.R
@@ -8,35 +8,99 @@ source("utils/function_box_plot.R")
 plot_SD_devStage <- ggplot(DATA_PARDOSES_30min_test[-125,], 
                              aes(x = DATA_PARDOSES_30min_test[-125,]$Dev_stage.x, 
                                  y = DATA_PARDOSES_30min_test[-125,]$SD)) +
-    geom_boxplot(fill="grey", alpha=0.8, outlier.shape = NA) + 
-    geom_sina(size=2.5, alpha=1, aes(shape=DATA_PARDOSES_30min_test[-125,]$Sex)) + 
+    geom_boxplot(fill="grey", 
+                 alpha=0.8, 
+                 outlier.shape = NA) + 
+    geom_sina(size=2.5, 
+              alpha=1, 
+              aes(shape=DATA_PARDOSES_30min_test[-125,]$Sex)) + 
     scale_shape_manual(values=c(1, 16)) +
     scale_color_manual(values =  c("grey", "grey")) + 
-    theme(legend.position = "none", panel.grid.major = element_blank(), panel.grid.minor = element_blank(),   panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
+    theme(legend.position = "none", panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),   
+          panel.background = element_blank(), 
+          axis.line = element_line(colour = "black")) + 
     labs(x = "Developmental stage",y =  "Tpref range (°C)")
-  
+
+plot_SD_devStage <- plot_SD_devStage + ggtitle('C')
+
 # Body size
 
-Plot_median_body <- regression_plot(DATA_PARDOSES_30min_test, DATA_PARDOSES_30min_test$Length_mm, "Body size (mm)", DATA_PARDOSES_30min_test$median, "median Tpref (°C)", DATA_PARDOSES_30min_test$Sex, "Sex")
-Plot_SD_body <- regression_plot(DATA_PARDOSES_30min_test[-125,], DATA_PARDOSES_30min_test[-125,]$Length_mm, "Body size (mm)", DATA_PARDOSES_30min_test[-125,]$SD, "Tpref range (°C)", DATA_PARDOSES_30min_test[-125,]$Sex, "Sex")
+Plot_mean_body <- regression_plot(DATA_PARDOSES_30min_test, 
+                                  DATA_PARDOSES_30min_test$Length_mm, 
+                                  "Prosoma length (mm)", 
+                                  DATA_PARDOSES_30min_test$mean, 
+                                  "mean Tpref (°C)", 
+                                  DATA_PARDOSES_30min_test$Sex, 
+                                  "Sex")
+
+Plot_mean_body <- Plot_mean_body + ggtitle('A')
+
+Plot_mean_body_adults <- regression_plot(DATA_PARDOSES_30min_test_adults, 
+                                         DATA_PARDOSES_30min_test_adults$Length_mm, 
+                                         "Prosoma length (mm)", 
+                                         DATA_PARDOSES_30min_test_adults$mean, 
+                                         "mean Tpref (°C)", 
+                                         DATA_PARDOSES_30min_test_adults$Sex, "Sex")
+
+Plot_mean_body_adults <- Plot_mean_body_adults + ggtitle('D')
+
+Plot_SD_body <- regression_plot(DATA_PARDOSES_30min_test[-125,], 
+                                DATA_PARDOSES_30min_test[-125,]$Length_mm, 
+                                "Prosoma length (mm)", 
+                                DATA_PARDOSES_30min_test[-125,]$SD, 
+                                "Tpref range (°C)", 
+                                DATA_PARDOSES_30min_test[-125,]$Sex, 
+                                "Sex")
+
+Plot_SD_body <- Plot_SD_body + ggtitle('B')
 
 # Sex
 
-Plot_SD_sex <- box_plot(DATA_PARDOSES_30min_test_adults, DATA_PARDOSES_30min_test_adults$Sex, "Sex", DATA_PARDOSES_30min_test_adults$SD, "Tpref range (°C)")
-Plot_SD_sex <- Plot_SD_sex + ggtitle('Adults')
+Plot_mean_sex <- box_plot(DATA_PARDOSES_30min_test_adults, 
+                          DATA_PARDOSES_30min_test_adults$Sex, 
+                          "Sex", 
+                          DATA_PARDOSES_30min_test_adults$mean, 
+                          "Mean Tpref (°C)")
+
+Plot_mean_sex <- Plot_mean_sex + ggtitle('E')
+
+Plot_SD_sex <- box_plot(DATA_PARDOSES_30min_test_adults, 
+                        DATA_PARDOSES_30min_test_adults$Sex, 
+                        "Sex", 
+                        DATA_PARDOSES_30min_test_adults$SD, 
+                        "Tpref range (°C)")
+
+Plot_SD_sex <- Plot_SD_sex + ggtitle('F')
 
 # UHI
 
-Plot_SD_UHI <- regression_plot(DATA_PARDOSES_30min_test_juveniles[-49,], DATA_PARDOSES_30min_test_juveniles[-49,]$UHI_03_04_pixel.x, "UHI intensity (°C)", DATA_PARDOSES_30min_test_juveniles[-49,]$SD, "Tpref range (°C)", DATA_PARDOSES_30min_test_juveniles[-49,]$Sex, "Sex")
-Plot_SD_UHI <- Plot_SD_UHI + ggtitle('Juveniles')
+Plot_SD_UHI <- regression_plot(DATA_PARDOSES_30min_test_juveniles[-49,], 
+                               DATA_PARDOSES_30min_test_juveniles[-49,]$UHI_03_04_pixel.x, 
+                               "UHI intensity (°C)", 
+                               DATA_PARDOSES_30min_test_juveniles[-49,]$SD, 
+                               "Tpref range (°C)", 
+                               DATA_PARDOSES_30min_test_juveniles[-49,]$Sex, 
+                               "Sex")
+
+Plot_SD_UHI <- Plot_SD_UHI + ggtitle('G')
+
+# Empty plot
+plot_blank <- ggplot() + theme_void()
 
 # Create figure
 
-fig2 <- (Plot_median_body | Plot_SD_body | plot_SD_devStage ) / (Plot_SD_sex | Plot_SD_UHI) + plot_layout(guides = "collect") + plot_annotation(tag_levels = 'A')
+fig2 <- (Plot_mean_body | Plot_SD_body | plot_SD_devStage ) / 
+  (Plot_mean_body_adults | Plot_mean_sex | Plot_SD_sex ) / 
+  (Plot_SD_UHI | plot_blank | plot_blank) + 
+  plot_layout(guides = "collect") + 
+  plot_annotation(tag_levels = list(c('                All individuals', '', '', 
+                                      '     Adults', '', '', 
+                                      '         Juveniles', '', ''))) & 
+  theme(plot.tag = element_text(size = 18))
 fig2
 
 # Export as jpeg file
-jpeg("outputs/fig2.jpg", width = 600, height = 500)
+jpeg("outputs/fig2.jpg", width = 3200, height = 2400, res = 300)
 fig2
 dev.off()
-
